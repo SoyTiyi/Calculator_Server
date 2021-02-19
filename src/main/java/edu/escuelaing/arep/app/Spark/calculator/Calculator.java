ca0903d9e36dfa83cfe1b461abcf2abd2028b7f6
SoyTiyi <santiago.marmartinez@gmail.com>
2021-02-19 14:37:20 -0500

    fix operation

--- src/main/java/edu/escuelaing/arep/app/Spark/calculator/Calculator.java
+++ src/main/java/edu/escuelaing/arep/app/Spark/calculator/Calculator.java
@@ -8,11 +8,15 @@ public class Calculator {
     public String returnJsonResponse(int value, String opera){
         Double result;
         Double newValue = Double.parseDouble(value+"");
-        if(opera == "sin"){
+        System.out.println(opera);
+        if(opera.equals("sin")){
+            System.out.println("SIN");
             result = Math.sin(newValue);
-        } else if(opera == "cos"){
+        } else if(opera.equals("cos")){
+            System.out.println("COS");
             result = Math.cos(newValue);
         }else {
+            System.out.println("TAN");
             result = Math.tan(newValue);
         }
         System.out.println(result);
